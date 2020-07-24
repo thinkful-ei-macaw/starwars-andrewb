07e152ac07c25688f90cc4059ff1f1c626f60d95
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-07-24 15:42:28 -0700

    added files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main className='App'>
+      <header>
+        {/* content goes here */}
+      </header>
+      
+    </main>
+  );
+}
+
+export default App;
