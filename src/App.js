6d377ac9a519ec22a387e9c0ab7f2352500e2a20
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-07-25 17:31:38 -0700

    heading and search criteria

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ function App() {
   return (
     <main className='App'>
       <header>
-        {/* content goes here */}
+        <h1>THE STAR WARS API</h1>
       </header>
+      <form>
+        <h2>ENTER YOUR STAR WARS SEARCH CRITERIA HERE:</h2>
+        <select>
+          <option>Planets</option>
+          <option>Spaceships</option>
+          <option>Vehicles</option>
+          <option>Characters</option>
+          <option>Films</option>
+          <option>Species</option>
+        </select>
+
+      </form>
       
     </main>
   );
